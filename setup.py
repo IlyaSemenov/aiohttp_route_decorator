ba2857e4a18ac1bf0595c47d812dea35404ce351
Ilya Semenov <semenov@inetss.com>
2017-07-12 14:33:49 +0700

    pypi 0.1.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='aiohttp_route_decorator',
-	version='0.1.3',
+	version='0.1.4',
 	url='https://github.com/IlyaSemenov/aiohttp_route_decorator',
 	license='BSD',
 	author='Ilya Semenov',
